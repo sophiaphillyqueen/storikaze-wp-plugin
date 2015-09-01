e8cde6befff2d7e470228a7e1a651e1c022f261a
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-09-01 15:51:49 -0600

    Fixed a VERY crucial typo

--- csfun/main.php
+++ csfun/main.php
@@ -2,7 +2,7 @@
 function storikaze_css_display ( ) {
   echo "<style>\n";
   storikaze_lcvar_file(dirname(__FILE__) . "/smalltx.php");
-  echo "\n<style>";
+  echo "\n</style>";
 }
 add_action('wp_head','storikaze_css_display');
 ?>
