7567d7d97b6a10901ac3bbd72b409b650b704392
Sophia Shapira <Sophia.Shapira@gmail.com>
2015-10-15 21:06:32 -0600

    Added td.storikaze_small_mesg style

--- csfun/smalltx.php
+++ csfun/smalltx.php
@@ -5,6 +5,11 @@ td.storikaze_small_anc {
 	font-variant: small-caps;
 }
 
+td.storikaze_small_mesg {
+	text-align: center;
+	font-size: 80%;
+}
+
 td.storikaze_small_adspc {
 	text-align: center;
 }
--- main-file.php
+++ main-file.php
@@ -3,7 +3,7 @@
  * Plugin Name: Storikaze Wordpress Plugin
  * Plugin URI: https://storikaze.org
  * Description: Gets WordPress to act as a Webserial fiction manager rather than a blog
- * Version: 0.0.4.17
+ * Version: 0.0.4.19d
  * Author: Sophia Elizabeth Shapira
  * Author URI: https://sshapira.com
  * License: GPL2+
